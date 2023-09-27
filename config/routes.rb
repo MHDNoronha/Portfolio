e585e11aa452776f58ea18a0e5bd094e3dfc4c84
MHDNoronha <mhdnoronha@aol.com>
2023-09-27 10:46:23 +0300

    picture moved to right, tabs now function, cv downloadable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'experience/index'
+  get 'cv/index'
+  get 'projects/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get '/homepage/home', to: 'home#index'
-  get 'experience', to: 'home#experience'
-  get 'cv', to: 'home#cv'
-  get 'projects', to: 'home#projects'
+  get '/projects', to: 'projects#index', as: 'projects'
+  get '/download_cv', to: 'cv#download', as: 'download_cv'
+  get '/experience', to: 'experience#index', as: 'experience'
 end
